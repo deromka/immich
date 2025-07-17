2bbd427f9c142376e8ad739f9df8c602958cef4d
Roman <romande@gmail.com>
2025-07-17 01:09:09 -0700

    added explicit usage of nvidia

--- run.bat
+++ run.bat
@@ -1,1 +1,13 @@
+@echo off
+cd /d D:\immich
+
+
+:retry
+docker info >nul 2>&1
+if errorlevel 1 (
+    echo Docker not ready, retrying in 10 seconds...
+    timeout /t 10
+    goto retry
+)
+
 docker compose -f immich.workers.yml --env-file .env up -d --scale immich-microservices=3
